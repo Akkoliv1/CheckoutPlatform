a622fa1ca1532a59833e9b328ede55ccbad88df6
akkoliv1 <ap.basanth@gmail.com>
2021-04-27 21:32:37 +0530

    Introduced Façade design pattern

---
+++ PromotionEngine/PresentationLayer/ConsoleLayer.cs
@@ -0,0 +1,68 @@
+﻿using PromotionEngine.Entities;
+using PromotionEngine.Interface;
+using PromotionEngine.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.PresentationLayer
+{
+   public class ConsoleLayer: IPromotionInputOutput
+    {
+        ConfigRepository configManagement;
+
+        public ConsoleLayer()
+        {
+            configManagement = new ConfigRepository();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<ProductCheckout> LoadUserInput()
+        {
+
+            List<ProductCheckout> checkoutList = new List<ProductCheckout>();
+            List<Product> lstProduct = LoadAvilableProducts();
+
+            Console.WriteLine("Enter User Inputs");
+            foreach (var item in lstProduct)
+            {
+                Console.WriteLine("Input quantity of " + item.ProductCode);
+                int quantity = Convert.ToInt32(Console.ReadLine());
+
+                checkoutList.Add(new ProductCheckout()
+                {
+                    ProductCode = item.ProductCode,
+                    Quantity = quantity,
+                    DefaultPrice = item.Price
+                });
+            }
+
+            return checkoutList;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private List<Product> LoadAvilableProducts()
+        {
+            return configManagement.GetAvilableProducts();
+        }
+        public bool DisplayTotalPrice(Entities.AppliedOffer appliedOffer)
+        {
+            Console.WriteLine("Calculating Final Price..........................");
+            Console.WriteLine("ProductCode" + "-" + "Quantity" + " - " + "FinalPrice" + " - " + "HasOffer");
+            foreach (var item in appliedOffer.Checkouts)
+            {
+                Console.WriteLine(item.ProductCode + "-" + item.Quantity + "-" + item.FinalPrice + "-" + item.HasOffer);
+            }
+            Console.WriteLine("Total Price : " + appliedOffer.TotalPrice);
+            return true;
+        }
+    }
+}
