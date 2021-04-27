5e7af61092e0979abd352e0f3969704dc3024065
akkoliv1 <ap.basanth@gmail.com>
2021-04-27 21:36:21 +0530

    Introduced Strategy pattern for Promotion

---
+++ PromotionEngine/PromotionStrategies/AdditionalItemOffer.cs
@@ -0,0 +1,23 @@
+﻿using PromotionEngine.Entities;
+using PromotionEngine.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.PromotionStrategies
+{
+    public class AdditionalItemOffer : IPromotionStrategy
+    {
+        public double CalculateProductPrice(List<ProductCheckout> productCheckoutList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanExecute(ProductCheckout product, List<Promotion> promotions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
